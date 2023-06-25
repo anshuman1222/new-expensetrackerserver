274e31f0be3e13dd578e5db353fd72a0bfd4f3a1
anshuman1222 <anshumanbehera9999@gmail.com>
2023-06-25 15:41:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express=require('express')
+const cors=require('cors')
+const {db} =require('./db/db')
+const {readdirSync}=require('fs')
+const app=express()
+require('dotenv').config()
+const PORT=process.env.PORT
+app.use(express.json())
+app.use(cors())
+readdirSync('./routes').map((route)=> app.use('/api',require('./routes/'+route)))
+const server=()=>{
+    db()
+    app.listen(PORT,()=>{
+        console.log(`You are listening to port : ${PORT}`)
+    })
+ 
+}
+server()
---
+++ models/incomeModel.js
@@ -0,0 +1,34 @@
+const mongoose =require('mongoose')
+const IncomeSchema= new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    amount: {
+        type: Number,
+        required: true,
+        trim:true
+    },
+    type: {
+        type: String,
+        default:"income"
+    },
+    date:{
+        type:Date,
+        required:true,
+        trim:true
+    },
+     category: {
+        type: String,
+        required: true,
+        trim:true
+    },
+    description:{
+        type:String,
+        required:true,
+    }
+},{timestamps:true})
+
+
+module.exports=mongoose.model('Income',IncomeSchema)
